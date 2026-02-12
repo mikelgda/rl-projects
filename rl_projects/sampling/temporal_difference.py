251c2c97e5f353f3a698dee4ee50fe8c4c1888d2
mikelgda <mikel.gdaa@gmail.com>
2026-02-12 23:08:51 +0100

    forward td lambda

--- rl_projects/sampling/temporal_difference.py
+++ rl_projects/sampling/temporal_difference.py
@@ -110,3 +110,59 @@ def tdn_policy_evaluation(
             V_per_episode[episode] = V
 
     return V, V_per_episode
+
+
+def forward_td_lambda_policy_evaulation(
+    env,
+    policy: np.ndarray,
+    gamma: float = 1.0,
+    lambda_val: float = 0.05,
+    initial_alpha: float = 0.5,
+    min_alpha: float = 0.01,
+    alpha_decay_rate: float = 0.5,
+    max_steps: int = 100,
+    n_episodes: int = 500,
+):
+    n_states = env.observation_space.n
+
+    alphas = decay_schedule(
+        initial_value=initial_alpha,
+        min_value=min_alpha,
+        decay_rate=alpha_decay_rate,
+        max_steps=n_episodes,
+    )
+
+    gamma_discount = np.logspace(
+        0, max_steps, num=max_steps, base=gamma, endpoint=False
+    )
+
+    V = np.zeros(env.observation_space.n)
+    V_per_episode = np.zeros((n_episodes, n_states))
+
+    for episode in range(n_episodes):
+        trajectory = collect_trajectory(env, policy, max_steps)
+        n = len(trajectory)
+
+        for j in range(n - 1):
+            est_state = trajectory[j, 0]
+            g_values = []
+            lambdas = []
+            for i in range(1 + j, n + 1):
+                rewards = trajectory[j:i, 2]
+                discounted_rewards = rewards * gamma_discount[j:i]
+                done = trajectory[i - 1, -2]
+                next_state = trajectory[i - 1, 3]
+                g = np.sum(discounted_rewards) + gamma_discount[i] * V[next_state] * (
+                    not done
+                )
+                g_values.append(g)
+                lambdas.append(
+                    (1 - (lambda_val if i < n else 0)) * (lambda_val ** (i - j - 1))
+                )
+
+            td_target = np.sum(np.array(g_values) * np.array(lambdas))
+            V[est_state] += alphas[episode] * (td_target - V[est_state])
+
+        V_per_episode[episode] = V
+
+    return V, V_per_episode
