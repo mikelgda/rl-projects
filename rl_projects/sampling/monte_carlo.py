b59d50515f1484f72d20196784278e94ca171d3a
mikelgda <mikel.gdaa@gmail.com>
2026-02-13 18:38:43 +0100

    sarsa

--- rl_projects/sampling/monte_carlo.py
+++ rl_projects/sampling/monte_carlo.py
@@ -117,6 +117,7 @@ def monte_carlo_control(
             choice_method=choice_method,
             max_steps=max_steps,
             episode=episode,
+            max_episodes=n_episodes,
             **choice_method_kwargs,
         )
 
@@ -132,6 +133,63 @@ def monte_carlo_control(
 
         Q_per_episode[episode] = Q
 
+        policy = np.zeros((n_states, n_actions), dtype=int)
+        policy[np.arange(n_states), np.argmax(Q, axis=1)] = 1
+        policy_per_episode[episode] = policy
+
+    V = np.max(Q, axis=1)
+    policy = policy_per_episode[-1]
+
+    return Q, V, policy, Q_per_episode, policy_per_episode
+
+
+def sarsa(
+    env: Any,
+    gamma: float = 1.0,
+    initial_alpha: float = 0.5,
+    min_alpha: float = 0.01,
+    alpha_decay_rate: float = 0.3,
+    n_episodes: int = 10_000,
+    choice_method: Callable = epsilon_greedy_choice,
+    **choice_method_kwargs,
+):
+
+    n_states = env.observation_space.n
+    n_actions = env.action_space.n
+
+    alphas = decay_schedule(
+        initial_value=initial_alpha,
+        min_value=min_alpha,
+        decay_rate=alpha_decay_rate,
+        max_steps=n_episodes,
+    )
+
+    Q = np.zeros((n_states, n_actions), dtype=np.float32)
+    Q_per_episode = np.zeros((n_episodes, n_states, n_actions), dtype=np.float32)
+
+    policy_per_episode = np.zeros((n_episodes, n_states, n_actions), dtype=np.int32)
+
+    for episode in tqdm(range(n_episodes)):
+
+        state, _ = env.reset()
+        done = False
+        action = choice_method(
+            state, Q, episode, max_episodes=n_episodes, **choice_method_kwargs
+        )
+
+        while not done:
+            next_state, reward, done, _, _ = env.step(action)
+            next_action = choice_method(
+                next_state, Q, episode, max_episodes=n_episodes, **choice_method_kwargs
+            )
+
+            td_target = reward + gamma * Q[next_state, next_action] * (not done)
+
+            Q[state, action] += alphas[episode] * (td_target - Q[state, action])
+
+            state, action = next_state, next_action
+
+        Q_per_episode[episode] = Q
         policy = np.zeros((n_states, n_actions), dtype=int)
         policy[np.arange(n_states), np.argmax(Q, axis=1)] = 1
         policy_per_episode[episode] = policy
