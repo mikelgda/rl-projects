e07269b71d35bf02b01955fc8882104ed775af8e
mikelgda <mikel.gdaa@gmail.com>
2026-02-15 00:51:02 +0100

    fix value iteration

--- rl_projects/basic/value_iteration.py
+++ rl_projects/basic/value_iteration.py
@@ -5,7 +5,7 @@ from numpy.typing import NDArray
 def value_iteration(
     mdp: dict[int, dict], gamma: float = 1.0, theta: float = 1e-10
 ) -> NDArray:
-    V = np.zeros(len(mdp))
+    V = np.zeros(len(mdp), dtype=np.float64)
 
     while True:
         Q = np.zeros((len(mdp), len(mdp[0])), dtype=np.float64)
@@ -13,7 +13,9 @@ def value_iteration(
         for state in mdp:
             for action in mdp[state]:
                 for prob, next_state, reward, done in mdp[state][action]:
-                    Q[state, action] += prob * (reward + gamma * V[next_state])
+                    Q[state, action] += prob * (
+                        reward + gamma * V[next_state] * (not done)
+                    )
 
         if np.max(np.abs(V - np.max(Q, axis=1))) < theta:
             break
